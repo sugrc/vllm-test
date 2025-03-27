327fe2bcae1c03790a6629d00089cefc4b6bbd09
sugrc <susana.garcia@atos.net>
2025-03-27 13:46:45 +0100

    First version. It works

--- existence_check.py
+++ existence_check.py
@@ -2,66 +2,123 @@ import requests
 import os
 import base64
 import logging
+import argparse
 
 logging.basicConfig(level=logging.INFO)
 
+parser = argparse.ArgumentParser()
 
-def existence_check(object: str, image_file: str):
+parser.add_argument(
+    '--path',
+    type=str,
+    required=True
+)
+parser.add_argument(
+    '--object',
+    type=str,
+    required=True
+)
+args = parser.parse_args()
+
+
+path = args.path
+logging.info("Path: %s", path)
+
+object = args.object
+logging.info("Object: %s", object)
+
+
+list_images = [file for file in os.listdir(path) if file.endswith((".jpg"))]
+logging.info("List of images: %s", list_images)
+
+
+def image_to_base64(image):
     """
-    Checks the existence of the given object in the image
+    Encodes an image to base64.
+    Params:
+        image: name of the image
+    Returns:
+        string of the image encoded in base64
     """
     try:
-        with open(image_file, "rb") as image:
+        with open(image, "rb") as image:
             image_bin = image.read()
         image_base64 = base64.b64encode(image_bin).decode("utf-8")
-        
-        ip_vllm = os.getenv('IP','http://vllm:8000') 
-        model = os.getenv('MODEL','HuggingfaceTB/SmolVLM-256M-Instruct')
-        url= ip_vllm + '/v1/chat/completions'
-
-        question = "Is " + object + " in the image?"
-
-        image_json = "data:image/jpg;base64," + str(image_base64)
-        payload = {
-            "model": model,
-            "messages": [
-                {
-                    "role": "system",
-                    "content": "You are a vision-language model."
-                },
-                {
-                    "role": "user",
-                    "content": "Describe this image",
-                    "image": image_json
-                }
-            ],
-            "max_tokens": 1
-        }
-
-        headers = {
-            "Content-Type": "application/json"
-        }
-        response = requests.post(url, json=payload, headers=headers)
-        logging.info("Request sent")
-
-        if response.status_code == 200:
-            print("Answer:", response.json())
-            # return checks if the response is either 'yes' or 'no'
-        else:
-            print(f"Error: {response.status_code}, {response.text}")
-
-
+        return image_base64
     except Exception as e:
         print(e)
 
+list_images_base64 = list(map(image_to_base64,list_images))
+logging.info("List of encoded images: %s",list_images_base64)
 
-path = os.getcwd()
-logging.info("Path: %s", path)
 
-image_path = path + '/images/star.jpg'
-logging.info("Image path: %s", image_path)
+ip_vllm = os.getenv('IP','http://vllm:8000') 
 
-existence_check("star", image_path)
+model = os.getenv('MODEL','HuggingfaceTB/SmolVLM-256M-Instruct')
+
+url= ip_vllm + '/v1/chat/completions'
+
+
+def make_a_payload(image_base64):
+    """
+    Given an encoded image, makes a payload in json format
+    """
+    question = "Is there " + str(object) + " in the image?"
+    logging.info("Question: %s", question)
+    image_json = "data:image/jpg;base64," + str(image_base64)
+    payload = {
+        "model": model,
+        "messages": [
+            {
+              "role": "system",
+                "content": "You are a vision-language model."
+            },
+            {
+                "role": "user",
+                "content": str(question),
+                "image": image_json
+            }
+        ],
+        "max_tokens": 50
+    }
+
+    return payload
+
+
+list_payload = list(map(make_a_payload,list_images_base64))
+logging.info("List of payload")
+
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+
+def existence_check(answer):
+    if ("yes" or "Yes") in answer:
+        return True
+    else: 
+        return False
+
+image_counter=0 #counter to show which image
+
+for payload in list_payload:
+    response = requests.post(url, json=payload, headers=headers)
+    logging.info("Request sent for file: %s",list_images[image_counter])
+    logging.info("Payload sent: %s", payload)
+    logging.info("URL: %s", url)
+    image_counter +=1
+    if response.status_code == 200:
+        logging.info("Answer: %s", response.json())
+        content = response.json()
+        logging.info(type(content))
+        answer = content['choices'][0]['message']['content']
+        bool_answer = existence_check(answer)
+        logging.info(bool_answer)
+        logging.info("Content: %s", str(content))
+    else:
+        logging.error("Error: %s, %s", response.status_code, response.text)
+
 
 
 """
@@ -71,7 +128,8 @@ Answer: {'id': 'chatcmpl-91684fc3ece941fe8a9fd3d50be34ad0',
  'created': 1742912756, 
  'model': 'HuggingfaceTB/SmolVLM-256M-Instruct', 
  'choices': [{'index': 0, 
-    'message': {'role': 'assistant', 
+    'message': 
+        {'role': 'assistant', 
         'reasoning_content': None, 
         'content': '<RESPONSE>', 
         'tool_calls': []}, 
@@ -83,5 +141,7 @@ Answer: {'id': 'chatcmpl-91684fc3ece941fe8a9fd3d50be34ad0',
 'completion_tokens': 50, 
 'prompt_tokens_details': None}, 
 'prompt_logprobs': None}
+
+So, it is a dictionary. To access to the message we c
 
 """
