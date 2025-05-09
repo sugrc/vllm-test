29ded9e855a7b02b1617d776bb4ae8559c9fd928
sugrc <susana.garcia@atos.net>
2025-05-09 08:40:04 +0200

    Changed local image for an online dummy image

--- tests/test_image_to_base64.py
+++ tests/test_image_to_base64.py
@@ -1,18 +1,20 @@
 import pytest
 import sys
 import base64
+import requests
 
 sys.path.append("/home/a939219/vllm-test")
 from realism_evaluation_system import image_to_base64
 
-image_path = "/home/a939219/vllm-test/images/bunny.jpg"
+image_url = "https://picsum.photos/200/300"
 
 
 def test_image_to_base64():
     try:
-        with open(image_path, "rb") as image_file:
-            expected_output = base64.b64encode(image_file.read()).decode("utf-8")
-            assert image_to_base64(image_path) == expected_output
+        response = requests.get(image_url)
+        response.raise_for_status()
+        expected_output = base64.b64encode(response.content).decode("utf-8")
+        assert image_to_base64(image_url) == expected_output
     except Exception as e:
         print(f"Error: {e}")
         return None
